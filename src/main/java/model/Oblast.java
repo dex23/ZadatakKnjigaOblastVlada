4acbdb14888f3a34b6e35922260d24245baae3ff
vladaB91 <vladabecelic@hotmail.com>
2017-01-20 16:52:19 +0100

    Uradjena klasa Oblast closes #5

--- src/main/java/model/Oblast.java
+++ src/main/java/model/Oblast.java
@@ -3,5 +3,75 @@ package model;
 /**
  * Created by user on 20.1.2017..
  */
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName = "oblast")
 public class Oblast {
+    public static final String POLJE_NAZIV="naziv";
+    public static final String POLJE_POCETNA_STRANA="pocetna_strana";
+
+
+    @DatabaseField(generatedId = true)
+    private int id;
+    @DatabaseField(columnName = POLJE_NAZIV,canBeNull = false)
+    private String naziv;
+    @DatabaseField(columnName = POLJE_POCETNA_STRANA,canBeNull = false)
+    private int pocetnaStrana;
+
+    @DatabaseField(foreign = true,foreignAutoRefresh = false)
+    private Knjiga knjiga;
+
+    public Oblast() {
+
+    }
+
+
+    public Oblast(String naziv, int pocetnaStrana) {
+        this.naziv = naziv;
+        this.pocetnaStrana = pocetnaStrana;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public int getPocetnaStrana() {
+        return pocetnaStrana;
+    }
+
+    public void setPocetnaStrana(int pocetnaStrana) {
+        this.pocetnaStrana = pocetnaStrana;
+    }
+
+    public Knjiga getKnjiga() {
+        return knjiga;
+    }
+
+    public void setKnjiga(Knjiga knjiga) {
+        this.knjiga = knjiga;
+    }
+
+    
+    @Override
+    public String toString() {
+        return "Oblast{" +
+                "id=" + id +
+                ", naziv='" + naziv + '\'' +
+                ", pocetnaStrana=" + pocetnaStrana +
+                '}';
+    }
 }
