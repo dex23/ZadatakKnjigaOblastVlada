d8304a736bf14448a67f343b7155f8ab996732f3
vladaB91 <vladabecelic@hotmail.com>
2017-01-20 17:20:42 +0100

    uradjen zadatak izmena vrednosti closes #8

--- src/main/java/zadaci/Zadatak3IzmenaVrednosti.java
+++ src/main/java/zadaci/Zadatak3IzmenaVrednosti.java
@@ -1,18 +1,50 @@
 package zadaci;
 
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import model.Knjiga;
+import model.Oblast;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by user on 20.1.2017..
  */
 public class Zadatak3IzmenaVrednosti {
+
+    static Dao<Knjiga,Integer> knjigaDao;
+    static Dao<Oblast,Integer> oblastDao;
+
     public static void main(String[] args){
         ConnectionSource connectionSource = null;
         try {
             connectionSource = new JdbcConnectionSource("jdbc:sqlite:avionRoba.db");
+            knjigaDao= DaoManager.createDao(connectionSource, Knjiga.class);
+            oblastDao=DaoManager.createDao(connectionSource, Oblast.class);
+
+            //Prikaz vrednosti tabele Oblast
+            List<Oblast> oblasti=oblastDao.queryForAll();
+            for(Oblast o:oblasti)
+                System.out.println("o = " + o);
+
+            //Pronalazenje oblasti koje za vrednost kolone naziv imaju
+            // vrednost "Activity klasa"
+            oblasti=oblastDao.queryForEq(Oblast.POLJE_NAZIV,"Activity klasa");
+            Oblast zaIzmenu=oblasti.get(0);//Preuzimamo prvi pronadjeni
+            //Menjamo vrednost atributa pocetnaStrana na 35
+            zaIzmenu.setPocetnaStrana(35);
+            //Cuvamo izmene u bazi, menja se vrednost kolone pocetna_strana
+            oblastDao.update(zaIzmenu);
+
+            /*Prikaz vrednosti tabele Oblast
+               da potvrdimo da je vrednost izmenjena
+             */
+            oblasti=oblastDao.queryForAll();
+            for(Oblast o:oblasti)
+                System.out.println("o = " + o);
         }catch (Exception e) {
             e.printStackTrace();
         }finally {
